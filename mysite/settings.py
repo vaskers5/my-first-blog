6a518bbdb7f3cc9136459ac46ffd089f5f6db155
vaskers5 <danil.krokodil@mail.ru>
2019-11-08 15:27:04 +0500

    db

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydjangoproject_db',
+        'USER': 'vaskers5',
+        'HOST': '127.0.0.1',
+        'PORT':5432,
+     
     }
 }
 
